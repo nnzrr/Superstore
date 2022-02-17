4f1e160e04291363f604494c1b66f683e8e696e5
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-02-17 10:03:41 +0700

    Delete standarize data format.sql

--- DATA CLEANING with SQL/standarize data format.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE public.nashville_housing 
-	ALTER COLUMN saledatetransformed TYPE DATE --convert a column to a different datatype
-	
-SELECT CAST(saledate AS DATE) AS saledateconverted
-	FROM public.nashville_housing --retrieve a new column with a different datatype
