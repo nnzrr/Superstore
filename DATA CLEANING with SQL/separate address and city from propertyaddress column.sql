ef203e12c6cd2ce3cd0d0cc6ee0d23b60ed0947b
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-02-17 10:20:50 +0700

    Delete separate address and city from propertyaddress column.sql

--- DATA CLEANING with SQL/separate address and city from propertyaddress column.sql
+++
@@ -1,4 +0,0 @@
-SELECT 	propertyaddress,
-		SUBSTRING (propertyaddress, 1, POSITION(',' IN propertyaddress) -1) AS address,
-		SUBSTRING (propertyaddress, STRPOS(propertyaddress, ',') +2) AS city
-			FROM public.nashville_housing
