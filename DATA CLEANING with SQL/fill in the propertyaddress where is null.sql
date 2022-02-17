eb8a2f533a8d5147d07d6dd5849b82b3e1d508e5
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-02-17 10:54:18 +0700

    Update fill in the propertyaddress where is null.sql

--- DATA CLEANING with SQL/fill in the propertyaddress where is null.sql
+++ DATA CLEANING with SQL/fill in the propertyaddress where is null.sql
@@ -1,9 +1,11 @@
-SELECT  aa.parcelid, 
-        aa.propertyaddress, 
+SELECT  aa.parcelid,
+	aa.uniqueid AS null_propertyaddress,
+	bb.uniqueid,
+	aa.propertyaddress AS null_propertyaddress, 
         bb.propertyaddress, 
-        COALESCE(aa.propertyaddress, bb.propertyaddress) AS propertyaddresstransformed 
+        COALESCE(aa.propertyaddress, bb.propertyaddress) AS null_propertyaddresstransformed 
   FROM nashville_housing aa
-  JOIN nashville_housing bb
+  INNER JOIN nashville_housing bb
   ON aa.parcelid = bb.parcelid
   AND aa.uniqueid != bb.uniqueid
       WHERE aa.propertyaddress IS NULL
