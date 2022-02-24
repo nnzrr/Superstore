a3e587bd054733c49889c74e7449e59d98ac5ce8
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-02-24 21:26:25 +0700

    Create query.sql

---
+++ COVID-19 Personal Project/total cases per million/query.sql
@@ -0,0 +1,9 @@
+SELECT 	aa.location,
+		    aa.total_cases, 
+		    CAST((SUM(aa.total_cases/cc.population) * 10 ^ 6) AS DECIMAL(21,3)) AS totalcasespermillion 
+			    FROM covid19case aa
+				  JOIN covid19death bb
+				  ON aa.caseid = bb.caseid
+				  JOIN covid19vaccinated cc
+				  ON cc.vacid = bb.vacid
+					  GROUP BY aa.location, aa.total_cases
