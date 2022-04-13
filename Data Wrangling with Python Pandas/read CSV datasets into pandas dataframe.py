4bef43c7752efd7d12e70a072a216ae9095b9acd
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-04-13 10:28:45 +0700

    Update read CSV datasets into pandas dataframe.py

--- Data Wrangling with Python Pandas/read CSV datasets into pandas dataframe.py
+++ Data Wrangling with Python Pandas/read CSV datasets into pandas dataframe.py
@@ -12,3 +12,27 @@ supsto.columns
 #       'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category',
 #      'Product Name', 'Sales', 'Quantity', 'Discount', 'Profit'],
 #     dtype='object')
+
+supsto.dtypes
+# Row ID             int64
+# Order ID          object
+# Order Date        object
+# Ship Date         object
+# Ship Mode         object
+# Customer ID       object
+# Customer Name     object
+# Segment           object
+# Country           object
+# City              object
+# State             object
+# Postal Code        int64
+# Region            object
+# Product ID        object
+# Category          object
+# Sub-Category      object
+# Product Name      object
+# Sales            float64
+# Quantity           int64
+# Discount         float64
+# Profit           float64
+# dtype: object
