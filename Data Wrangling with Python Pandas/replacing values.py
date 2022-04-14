11cd2f7f0b4ae4e3903a0ea8629736c2ea6b5470
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-04-14 11:08:56 +0700

    Update replacing values.py

--- Data Wrangling with Python Pandas/replacing values.py
+++ Data Wrangling with Python Pandas/replacing values.py
@@ -11,3 +11,10 @@ ShipModes.drop_duplicates()
 # 35        First Class
 # 366          Same Day
 # Name: ShipMode, dtype: object
+
+# converting data without changing the data, to change the data using inplace=True
+rename_ShipMode = supsto.ShipMode.replace({'Second Class': 'Regular',
+                                           'Standard Class': 'Economy',
+                                            'First Class': 'Prime'}, inplace=False)
+rename_ShipMode.unique()
+# array(['Regular', 'Economy', 'Prime', 'Same Day'], dtype=object)
