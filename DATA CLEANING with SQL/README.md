d27fd1fc1a57ce9882882c1aeb26ee57312bd877
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-04-14 19:02:17 +0700

    Delete DATA CLEANING with SQL directory

--- DATA CLEANING with SQL/README.md
+++
@@ -1,1 +0,0 @@
-## Data Cleaning with SQL (POSTGRESQL)
--- DATA CLEANING with SQL/standarize data format/query.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE public.nashville_housing 
-	ALTER COLUMN saledate TYPE DATE --convert a column to a different datatype
-	
-SELECT CAST(saledate AS DATE) AS saledatetransformed
-	FROM public.nashville_housing --retrieve a new column with a different datatype
